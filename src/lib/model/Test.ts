275d965cfc1280bbdbb2eb65a0d42c4d891b16fc
YashPatel-250702 <yash.patel@tekworks.in>
2025-08-07 11:32:08 +0530

    Added test model

---
+++ src/lib/model/Test.ts
@@ -0,0 +1,15 @@
+export interface Test {
+  id: string;
+  name: string;
+  description: string;
+  conceptsCovered: string;
+  type: 'COLLEGE' | 'PLACEMENT'; 
+  status: 'ACTIVE' | 'INACTIVE'; 
+  duration: number; 
+  noOfQuestions: number;
+  noOfAttempts: number;
+  createdAt: string; 
+  updatedAt: string; 
+  mentorId: string;
+  publicLink: string | null;
+}
