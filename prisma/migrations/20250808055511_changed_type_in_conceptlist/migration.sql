885d2622eaff9d66c6486bb07619d415c9c3f692
YashPatel-250702 <yash.patel@tekworks.in>
2025-08-08 12:22:18 +0530

    Addded migration

--- prisma/migrations/20250808055511_changed_type_in_conceptlist/migration.sql
+++ prisma/migrations/20250808055511_changed_type_in_conceptlist/migration.sql
@@ -1,9 +1,3 @@
-/*
-  Warnings:
 
-  - The `conceptsCovered` column on the `Test` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
 ALTER TABLE "Test" DROP COLUMN "conceptsCovered",
 ADD COLUMN     "conceptsCovered" TEXT[];
